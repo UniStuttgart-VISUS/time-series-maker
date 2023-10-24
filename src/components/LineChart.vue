989768a5f46a208eae6ec1baf8588e475154747c
ArielMant0 <franziska.becker@vis.uni-stuttgart.de>
2023-10-24 14:43:16 +0200

    made line chart use canvas

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -7,7 +7,10 @@
                 variant="outlined"
                 @click="resetZoom"/>
         </div>
-        <svg ref="el" :width="width" :height="height"></svg>
+        <div style="position: relative;">
+            <canvas ref="el" :width="width" :height="height"></canvas>
+            <svg ref="overlay" :width="width" :height="height" class="overlay"></svg>
+        </div>
     </div>
 </template>
 
@@ -61,28 +64,32 @@
     });
 
     const el = ref(null);
+    const overlay = ref(null);
 
     const x = d => d[props.xAttr]
     const y = d => d[props.yAttr]
     const color = d => d[props.colorAttr]
     const id = d => d[props.idAttr]
 
-    let xScale, yScale, paths, zoom;
+    let ctx, line, zoom;
+    let xScale, yScale;
+    let xDomain, yDomain;
 
     const app = useApp();
 
     function draw() {
-        const svg = d3.select(el.value);
-
+        ctx = el.value.getContext("2d")
+        const svg = d3.select(overlay.value);
         svg.selectAll("*").remove();
+        ctx.clearRect(0, 0, props.width, props.height);
 
         if (props.data.length === 0) return;
 
-        const xDomain = props.xDomain ? props.xDomain : [
+        xDomain = props.xDomain ? props.xDomain : [
                 d3.min(props.data, d => d3.min(d.values, dd => x(dd))),
                 d3.max(props.data, d => d3.max(d.values, dd => x(dd))),
             ]
-        const yDomain = props.yDomain ? props.yDomain : [
+        yDomain = props.yDomain ? props.yDomain : [
                 Math.min(0, d3.min(props.data, d => d3.min(d.values, dd => y(dd)))),
                 d3.max(props.data, d => d3.max(d.values, dd => y(dd))),
             ]
@@ -104,28 +111,13 @@
             .domain(yDomain)
             .range([props.height - 25, 10])
 
-        const line = d3.line()
+        line = d3.line()
+            .context(ctx)
             .curve(d3.curveMonotoneX)
             .x(d => xScale(x(d)))
             .y(d => yScale(y(d)))
 
-        const zeroLine = svg.append("line")
-            .attr("x1", xScale(xDomain[0]))
-            .attr("x2", xScale(xDomain[1]))
-            .attr("y1", yScale(0))
-            .attr("y2", yScale(0))
-            .attr("stroke", "black")
-            .attr("stroke-width", 1)
-
-        paths = svg.append("g")
-            .selectAll("path")
-            .data(props.data)
-            .join("path")
-            .attr("d", d => line(d.values))
-            .attr("stroke", d => props.colorScale(color(d)))
-            .attr("stroke-opacity", d => d.opacity)
-            .attr("stroke-width", 2)
-            .attr("fill", "none")
+        highlight();
 
         const xAxis = svg.append("g")
             .attr("transform", `translate(0,${props.height-25})`)
@@ -143,33 +135,30 @@
                 .x(d => xz(x(d)))
                 .y(d => yz(y(d)))
 
-            paths.attr("d", d => line(d.values))
+            highlight(xz, yz);
 
             xAxis.call(d3.axisBottom(xz).tickFormat(d3.timeFormat("%b")))
             yAxis.call(d3.axisLeft(yz))
 
-            zeroLine
-                .attr("x1", xz(xDomain[0]))
-                .attr("x2", xz(xDomain[1]))
-                .attr("y1", yz(0))
-                .attr("y2", yz(0))
-
             app.setLineChartZoom(transform);
+        }
+
+        function zoomEnd({ transform }) {
+            // TODO: why do I have to do this manually?
+            svg.node().__zoom = transform;
         }
 
         zoom = d3.zoom()
             .scaleExtent([1, 8])
             .translateExtent([[0, 0], [props.width, props.height]])
             .on("zoom", zoomed)
+            .on("end", zoomEnd)
 
-        svg.call(zoom)
-            .on("dblclick.zoom", resetZoom)
+        svg.call(zoom).on("dblclick.zoom", resetZoom)
 
         svg.transition()
             .duration(500)
             .call(zoom.transform, app.lineChartZoom)
-
-        highlight()
     }
 
     function resetZoom() {
@@ -179,24 +168,38 @@
             .call(zoom.transform, d3.zoomIdentity)
     }
 
-    function highlight() {
+    function highlight(scaleX=xScale, scaleY=yScale) {
+        ctx.clearRect(0, 0, props.width, props.height);
 
-        if (app.selectedComps.size === 0) {
-            const domain = props.colorScale.domain();
-            paths
-                .attr("stroke-opacity", d => d.opacity)
-                .sort((a, b) => domain.indexOf(color(a)) - domain.indexOf(color(b)))
-        } else {
-            paths
-                .attr("stroke-opacity", d => app.isSelectedComponent(id(d)) ? 1 : d.opacity)
-                .filter(d => app.isSelectedComponent(id(d)))
-                .raise()
-        }
+        ctx.lineWidth = 1;
+        ctx.globalAlpha = 1;
+        ctx.strokeStyle = "black";
+        ctx.beginPath();
+        ctx.moveTo(scaleX(xDomain[0]), scaleY(0));
+        ctx.lineTo(scaleX(xDomain[1]), scaleY(0));
+        ctx.stroke()
+
+        ctx.lineWidth = 2;
+        props.data.forEach(d => {
+            ctx.beginPath();
+            ctx.globalAlpha = app.isSelectedComponent(id(d)) ? 1 : d.opacity;
+            ctx.strokeStyle  = props.colorScale(color(d));
+            line(d.values)
+            ctx.stroke();
+        });
     }
 
     onMounted(draw);
 
     watch(() => props.data, draw, { deep: true })
     watch(() => [props.width, props.height, props.xDomain, props.yDomain], draw, { deep: true })
-    watch(() => app.selectedComps, highlight, { deep: true })
+    watch(() => app.selectedComps, () => highlight(), { deep: true })
 </script>
+
+<style scoped>
+.overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+</style>
